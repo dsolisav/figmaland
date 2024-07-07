4a15678f6e1f034311b5189971b2a708aaf6e15e
dsolisav <dsolis@unal.edu.co>
2024-07-07 04:46:39 -0500

    Added form outline on focus

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -6,13 +6,13 @@ function ContactCard() {
                     Contact Us
                 </p>
                 <div>
-                    <input className="w-[85%] h-10 bg-[#F5F5F5] focus:outline-none border-[1px] border-[#E8E8E8] rounded-3xl py-2 px-4 text-xs placeholder-[#18171D]" placeholder="Your Name" />
+                    <input className="w-[85%] h-10 bg-[#F5F5F5] border-[1px] border-[#E8E8E8] rounded-3xl py-2 px-4 text-xs placeholder-[#18171D] focus:outline-zinc-400" placeholder="Your Name" />
                 </div>
                 <div>
-                    <input className="w-[85%] h-10 bg-[#F5F5F5] focus:outline-none border-[1px] border-[#E8E8E8] rounded-3xl py-2 px-4 text-xs placeholder-[#18171D]" placeholder="Your Email" />
+                    <input className="w-[85%] h-10 bg-[#F5F5F5] border-[1px] border-[#E8E8E8] rounded-3xl py-2 px-4 text-xs placeholder-[#18171D] focus:outline-zinc-400" placeholder="Your Email" />
                 </div>
                 <div>
-                    <textarea id="message" rows="4" className="w-[85%] bg-[#F5F5F5] focus:outline-none border-[1px] border-[#E8E8E8] p-4 text-xs placeholder-[#18171D] h-40" placeholder="Your message"></textarea>
+                    <textarea id="message" rows="4" className="w-[85%] bg-[#F5F5F5] border-[1px] border-[#E8E8E8] p-4 text-xs placeholder-[#18171D] h-40 focus:outline-zinc-400" placeholder="Your message"></textarea>
                 </div>
                 <div className="flex justify-start px-5">
                     <button className="bg-[#2091F9] hover:bg-[#196ebe] rounded-3xl text-white text-sm font-light py-[0.6rem] px-9" onClick={() => alert('Send')}>
