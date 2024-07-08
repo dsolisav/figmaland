4ed2d5ee029b6f8d1cab750e513b43d4a5c9550e
dsolisav <dsolis@unal.edu.co>
2024-07-07 23:45:36 -0500

    Polished minor responsive adjustment

--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -9,32 +9,32 @@ function ContactRow() {
         <div className="flex flex-col ssm:flex-row gap-9 justify-start ssm:justify-center flex-wrap items-start ssm:items-center">
 
             {/* Block of location image and information  */}
-            <div className="flex flex-row ssm:flex-col gap-2 justify-center text-left ssm:text-center items-center">
+            <div className="flex flex-row ssm:flex-col gap-5 ssm:gap-2 justify-center text-left ssm:text-center items-center">
                 <div className="flex justify-center">
                     <img src={locationImage} className='min-w-6 w-6' alt="appImage"/>
                 </div>
-                <p className='text-[#374754] text-xs font-light'>
+                <p className='text-[#374754] text-sm ssm:text-xs font-light'>
                     683 Spring St undefined Anchorage,<br />
                     Georgia 12473 United States
                 </p>
             </div>
 
             {/* Block of phone image and information  */}
-            <div className="flex flex-row ssm:flex-col gap-2 justify-center text-left ssm:text-center items-center">
+            <div className="flex flex-row ssm:flex-col gap-5 ssm:gap-2 justify-center text-left ssm:text-center items-center">
                 <div className="flex justify-center">
                     <img src={phoneImage} className='w-5' alt="appImage"/>
                 </div>
-                <p className='text-[#374754] text-xs font-light'>
+                <p className='text-[#374754] text-sm ssm:text-xs font-light'>
                     (843) 555-0130
                 </p>
             </div>
 
             {/* Block of mail image and information  */}
-            <div className="flex flex-row ssm:flex-col gap-2 justify-center text-left ssm:text-center items-center">
+            <div className="flex flex-row ssm:flex-col gap-5 ssm:gap-2 justify-center text-left ssm:text-center items-center">
                 <div className="flex justify-center">
                     <img src={mailImage} className='w-5' alt="appImage"/>
                 </div>
-                <p className='text-[#374754] text-xs font-light'>
+                <p className='text-[#374754] text-sm ssm:text-xs font-light'>
                     willie.jennings@example.com
                 </p>
             </div>
